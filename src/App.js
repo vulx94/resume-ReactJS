1f7cf9bfaeec10c6539972c7fcf48f8f73d3126a
Khoa Le\admin <le.khoa2209@gmail.com>
2017-06-26 12:34:54 +0700

    Finished product

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Experience from './Components/Experience';
 import Projects from './Components/Projects';
 import Skillsets from './Components/Skillsets';
 import Educations from './Components/Educations';
+
 import './App.css';
 
 class App extends Component {
@@ -17,17 +18,19 @@ class App extends Component {
                 <div className="profile-container">
                     <img className="profile img-responsive" src="assets/images/2014-07-24 14.55.12.jpeg" alt="" />
                     <h1 className="name">{resume.name}</h1>
-                    <h3 className="tagline">Front-End Develoer</h3>
+                    <h3 className="tagline">Fresher Front-End Developer</h3>
                 </div>
 
                 <div className="contact-container container-block">
                     <ul className="list-unstyled contact-list">
                         <li className="email"><i className="fa fa-envelope"></i><a href="mailto: le.khoa2209@gmail.com">{resume.email}</a></li>
+                        <li className="phone"><i className="fa fa-home"></i><a href={resume.addressURL}>{resume.address}</a></li>
                         <li className="phone"><i className="fa fa-phone"></i><a href="tel:+84 933991839">{resume.phone}</a></li>
                         <li className="github"><i className="fa fa-github"></i><a href={resume.gitHubURL} target="_blank">{resume.gitHub}</a></li>
                         <li className="twitter"><i className="fa fa-facebook"></i><a href={resume.facebookURL} target="_blank">{resume.facebook}</a></li>
                     </ul>
                 </div>
+
                 <div className="education-container container-block">
                     <h2 className="container-block-title">Education</h2>
                     {resume.education.map((item,index) =>{
@@ -35,6 +38,7 @@ class App extends Component {
                     <Educations {...item}/>
                     )
                     })}
+
                 </div>
 
                 <div className="languages-container container-block">
@@ -57,13 +61,29 @@ class App extends Component {
 
             </div>
 
+
             <div className="main-wrapper">
                 <section className="section summary-section">
-                    <h2 className="section-title"><i className="fa fa-user"></i>Career Profile</h2>
+                    <h2 className="section-title"><i className="fa fa-user"></i>Qualifications Summary</h2>
                     <div className="summary">
-                        <p>Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can <a href="http://themes.3rdwavemedia.com/website-templates/orbit-free-resume-cv-template-for-developers/" target="_blank">download this free resume/CV template here</a>. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.</p>
+                      <ul>
+                        <li>Have a passion about web developper. </li>
+                        <li>Always persue perfection and always finish the work on time. </li>
+                        <li>Love to work with ReactJS to build beautiful and functional Web App.</li>
+                        <li>Willing to learn and try my best to become a solid developper in the furture. </li>
+                      </ul>
                     </div>
                 </section>
+
+                <section className="section summary-section">
+                  <h2 className="section-title"><i className="fa fa-file"></i>About Myself</h2>
+                  <div className="summary">
+                    <p>My passion for web development has grown for over a year and I have enroll many of online course on Udemy and
+            www.freecodecamp.com.  Via  Udemy  I  have  create  a  full  working  website  project  that  is  YelpCamp  which  is  using
+            Bootstrap for the front-end to make it responsive on any device and using NodeJS, ExpressJS and MongoDB for the
+            Database  to  serve  on  the  Back-end. With  those  hours  of  studies  and  experience  on  my  own, beside that I have study and research more and more about ReacjJS, a very powerful tools to create such a beautiful and functional Web App and also responsive on most of the mobile devices.      </p>
+                        </div>
+                    </section>
 
                 <section className="section experiences-section">
                     <h2 className="section-title"><i className="fa fa-briefcase"></i>Experiences</h2>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,7 +4,8 @@ import React , {Component} from 'react';
 const Projects = (props) => {
   return (
     <div className="item">
-        <span className="project-title"><a href="#hook">{props.name}</a></span> - <span className="project-tagline">{props.detail}</span>
+        <span className="project-title"><a href="#hook">{props.name}</a><br />
+      </span> _ <span className="project-tagline">{props.detail}</span>
     </div>
   )
 }
